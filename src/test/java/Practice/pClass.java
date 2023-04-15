72ff6b7cdc066da06845de5f901984095a6bcb50
FahimImtiaz01 <fimtiaz_14@ihsnyc.org>
2023-04-15 12:45:12 -0400

    test

--- src/test/java/Practice/pClass.java
+++ src/test/java/Practice/pClass.java
@@ -1,7 +1,9 @@
 package Practice;
 
+import java.util.ArrayList;
+
 public class pClass {
     public static void main(String[] args) {
-
+ArrayList<String> names = new ArrayList<>();
     }// end of class
 }// end of main
